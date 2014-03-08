5041f8910ad6beabad72105af3e81f9e003fdca2
zhangjiji <programmer.zhangjiji@gmail.com>
2014-03-08 21:11:10 +0800

    curry uncurry

--- ch1/ex.hs
+++ ch1/ex.hs
@@ -8,3 +8,10 @@ greater (x,y) = if x > y then x else y
 
 areaOfCircle :: Double -> Double
 areaOfCircle r = r * r * 22 / 7
+
+uncurry :: (a -> b -> c) -> ((a,b)->c)
+uncurry f (x,y) = f x y
+
+curry :: ((a,b)->c) -> (a->b->c)
+curry f x y = f (x,y)
+
