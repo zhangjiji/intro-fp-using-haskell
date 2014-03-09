e25cad30cb020d1a4920f13f7e36e8a5b870dbe0
zhangjiji <programmer.zhangjiji@gmail.com>
2014-03-09 11:37:42 +0800

    ex 1.5.1, 1.5.2

--- ch1/ex.hs
+++ ch1/ex.hs
@@ -15,3 +15,17 @@ uncurry f (x,y) = f x y
 curry :: ((a,b)->c) -> (a->b->c)
 curry f x y = f (x,y)
 
+integral :: a -> b -> (r -> r)
+integral a b f = undefined
+
+fib :: Int -> Int
+fib n
+  | n == 0 = 0
+  | n == 1 = 1
+  | otherwise = (fib (n - 1)) + (fib (n - 2))
+
+abs :: Int -> Int
+abs n
+  | n < 0 = (-n)
+  | n == 0 = 0
+  | n > 0 = n
