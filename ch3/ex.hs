b97e00847df95b94f25d2fecbd89b650a6a0de4c
zhangjiji <programmer.zhangjiji@gmail.com>
2014-03-12 09:32:22 +0800

    ch3 ex

---
+++ ch3/ex.hs
@@ -0,0 +1,18 @@
+
+data Nat = Zero | Succ Nat
+         deriving (Eq, Ord, Show)
+
+--3.1.5
+minus :: Nat -> Nat -> Nat
+minus (Succ n) Zero = Succ n
+minus (Succ n) (Succ m)
+  | n < m = Zero
+  | otherwise = minus n m
+
+--3.1.1 Positive Numbers
+--data PNum = Nat | 
+  
+--3.1.2
+convert :: Nat -> Integer
+convert Zero = 0
+convert (Succ n) = 1 + convert n
