94d11b143dd2419f28b6d82a8799d612244438c4
zhangjiji <programmer.zhangjiji@gmail.com>
2014-04-30 16:51:19 +0800

    scan fusion

--- ch4/ex.hs
+++ ch4/ex.hs
@@ -24,5 +24,14 @@ tails (x:xs) = (x:xs) : (tails xs)
 filter' p = foldr f []
   where f x xs = if p x then x : xs else xs
 
+dot x y = max 0 (x+y)
+
+maxlist :: Ord a => [a] -> a
+maxlist = foldr1 max
+
+segs = concat . map inits . tails
+
+mss = maxlist . scanr dot 0
+
 main :: IO ()
 main = return ()
